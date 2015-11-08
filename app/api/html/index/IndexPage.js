c1cae226b4b0046f15bc2b974e48fdb0c29b03f5
bastien03 <bastienmary@gmail.com>
2015-11-08 20:55:37 +0100

    show list of directors

--- app/api/html/index/IndexPage.js
+++ app/api/html/index/IndexPage.js
@@ -3,9 +3,38 @@ module.exports = function(db) {
     var jade = require('jade');
     var moviesService = require('../../../services/MoviesService.js')(db);
 
+    var getDirectors = function(movies) {
+        var tmp_directors = [];
+        var directors = [];
+        for (var idx in movies) {
+            var director = movies[idx].director;
+            if (tmp_directors[director]) {
+                tmp_directors[director] = tmp_directors[director] + 1;
+            } else {
+                tmp_directors[director] = 1;
+            }
+        }
+
+        for (var name in tmp_directors) {
+            if (name == 'undefined') continue;
+
+            directors.push({
+                name: name,
+                numberMovies: parseInt(tmp_directors[name])
+            });
+        }
+
+        return directors.sort(function(a,b) {
+            return b.numberMovies - a.numberMovies
+        });
+    };
+
     var render = function(req, res) {
+        var movies = moviesService.getMovies(req.query);
+
         var configuration = {
-            movies: moviesService.getMovies(req.query),
+            movies: movies,
+            directors: getDirectors(movies),
             isAuthenticated: req.user
         };
         var html = jade.renderFile('app/api/html/index/index.jade', configuration);
