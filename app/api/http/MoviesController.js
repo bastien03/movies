44c20863b827866cdb8192b6585c97aba3241c63
bastien03 <bastienmary@gmail.com>
2015-11-08 17:56:47 +0100

    login

--- app/api/http/MoviesController.js
+++ app/api/http/MoviesController.js
@@ -3,11 +3,17 @@ module.exports = function (db) {
     var moviesService = require('../../services/MoviesService.js')(db);
 
     var addMovie = function (req, res) {
+        if (!req.user) {
+            return res.status(401).send();
+        }
         moviesService.addMovie(req.body);
         return res.redirect('/');
     };
 
     var deleteMovie = function (req, res) {
+        if (!req.user) {
+            return res.status(401).send();
+        }
         moviesService.deleteMovie(req.params.id);
         return res.status(200).send();
     };
