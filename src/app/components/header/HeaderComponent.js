da783accd7ed9f739708c5572def9b00396111d1
bastien03 <bastienmary@gmail.com>
2016-06-27 21:56:45 +0200

    HeaderComponent is now pure

--- src/app/components/header/HeaderComponent.js
+++ src/app/components/header/HeaderComponent.js
@@ -2,30 +2,23 @@ import React from 'react';
 import { Link } from 'react-router';
 import uris from '../../../uris';
 
-class HeaderComponent extends React.Component {
+const HeaderComponent = ({ isAuthenticated }) => (
+  <div>
 
-  render() {
-    const { isAuthenticated } = this.props;
+    <h1>Movies</h1>
 
-    return (
+    {isAuthenticated &&
       <div>
-
-        <h1>Movies</h1>
-
-        {isAuthenticated &&
-          <div>
-            <a href={uris.logoutApi()} className="pageLink">logout</a>
-            <Link to={uris.newMoviePage()} className="pageLink">add a movie</Link>
-          </div>
-        }
-
-        {!isAuthenticated &&
-          <Link to={uris.loginPage()} className="pageLink">login</Link>
-        }
+        <a href={uris.logoutApi()} className="pageLink">logout</a>
+        <Link to={uris.newMoviePage()} className="pageLink">add a movie</Link>
       </div>
-    );
-  }
-}
+    }
+
+    {!isAuthenticated &&
+      <Link to={uris.loginPage()} className="pageLink">login</Link>
+    }
+  </div>
+);
 
 HeaderComponent.propTypes = {
   isAuthenticated: React.PropTypes.object,
