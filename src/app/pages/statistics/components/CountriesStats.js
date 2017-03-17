c0c4cc6fb1211822098efe182ad259eb2f5e59ee
bastien03 <bastienmary@gmail.com>
2017-03-18 00:19:12 +0100

    improve statistics page

---
+++ src/app/pages/statistics/components/CountriesStats.js
@@ -0,0 +1,75 @@
+import React from 'react';
+
+import { Treemap, ResponsiveContainer } from 'recharts';
+import Please from 'pleasejs';
+
+const getColors = length => Please.make_color({
+  colors_returned: length,
+  base_color: 'red'
+});
+
+const CustomizedContent = React.createClass({
+  render() {
+    const { root, depth, x, y, width, height, index, payload, colors, rank, name, value } = this.props;
+    return (
+      <g>
+        <rect
+          x={x}
+          y={y}
+          width={width}
+          height={height}
+          style={{
+            fill: depth < 2 ? colors[index] : 'none',
+            stroke: '#fff',
+            strokeWidth: 2 / (depth + 1e-10),
+            strokeOpacity: 1 / (depth + 1e-10),
+          }}
+        />
+        {
+          depth === 1 ?
+          <text
+            x={x + width / 2}
+            y={y + height / 2 + 7}
+            textAnchor="middle"
+            fill="#fff"
+            fontSize={14}
+          >
+            {name}
+          </text>
+          : null
+        }
+      </g>
+    );
+  }
+});
+
+class CountriesStats extends React.Component {
+  render() {
+    const {stats} = this.props;
+    if (!stats || stats.length == 0) {
+      return null;
+    }
+    const data = [];
+    stats.map(country => data.push(
+      {
+        name: `${country._id} (${country.count})`,
+        children: [
+          { name: country._id, size: country.count },
+        ]
+      })
+    );
+    const colors = getColors(data.length);
+    return (
+      <ResponsiveContainer height="50%">
+        <Treemap
+          data={data}
+          dataKey="size"
+          stroke="#fff"
+          content={<CustomizedContent colors={colors}/>}
+        />
+      </ResponsiveContainer>
+    );
+  }
+}
+
+export default CountriesStats;
